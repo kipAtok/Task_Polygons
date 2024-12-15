b7abf4d4767e11dbc5b06f1411ea434e9664a664
NenVan155 <i.nenov155@gmail.com>
2024-12-15 16:46:09 +0300

    Task_2 homework done

--- Task_2/Task_2/CustomControl.cs
+++ Task_2/Task_2/CustomControl.cs
@@ -1,6 +1,5 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Media;
-using System.Collections.Generic;
 
 namespace Task_2
 {
@@ -8,11 +7,11 @@ namespace Task_2
     {
         public override void Render(DrawingContext drawingContext)
         {
-            List<Shape> shapes = [new Circle(100, 100), new Square(200, 100), new Triangle(300, 100),
-                                    new Circle(100, 200), new Circle(200, 200), new Square(200, 200), new Circle(300, 200), new Triangle(300, 200),
-                                    new Circle(200, 300), new Square(200, 300), new Triangle(200, 300)];
+            Shape[] shapes = [new Circle(100, 100), new Square(200, 100), new Triangle(300, 100),
+                                new Circle(100, 200), new Circle(200, 200), new Square(200, 200), new Circle(300, 200), new Triangle(300, 200),
+                                new Circle(200, 300), new Square(200, 300), new Triangle(200, 300)];
 
-            foreach (Shape shape in shapes)
+            foreach (var shape in shapes)
             {
                 shape.Draw(drawingContext);
             }
